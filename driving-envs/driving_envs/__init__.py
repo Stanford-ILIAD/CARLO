86354684da1f07b7612d305a3f3e3674879a5570
Allan Zhou <8732147+AllanYangZhou@users.noreply.github.com>
2019-09-02 16:12:34 -0700

    Update max_episode_steps 300->200

--- driving-envs/driving_envs/__init__.py
+++ driving-envs/driving_envs/__init__.py
@@ -1,4 +1,4 @@
 from gym.envs.registration import register
 
-register(id="Merging-v0", entry_point="driving_envs.envs:MergingEnv", max_episode_steps=300)
+register(id="Merging-v0", entry_point="driving_envs.envs:MergingEnv", max_episode_steps=200)
 register(id="Turning-v0", entry_point="driving_envs.envs:TurningEnv")
