b64830033e535748232d90b6563c712b0ae0ac56
Allan Zhou <8732147+AllanYangZhou@users.noreply.github.com>
2019-09-05 13:44:56 -0700

    Allow git configuring test script

--- test_single.py
+++ test_single.py
@@ -1,11 +1,20 @@
 import argparse
 import os
 import time
+import gin
 import gym
 import numpy as np
 from single_agent_env import make_single_env
 from stable_baselines import PPO2
 from stable_baselines.common.vec_env import DummyVecEnv, vec_normalize
+from tensorflow import flags
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("name", None, "Name of experiment")
+flags.DEFINE_multi_string("gin_file", None, "List of paths to the config files.")
+flags.DEFINE_multi_string(
+    "gin_param", None, "Newline separated list of Gin parameter bindings."
+)
 
 
 def test(experiment_name):
@@ -34,7 +43,5 @@ def test(experiment_name):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("experiment_name", type=str)
-    args = parser.parse_args()
-    test(args.experiment_name)
+    gin.parse_config_files_and_bindings(FLAGS.gin_file, FLAGS.gin_param)
+    test(FLAGS.name)
