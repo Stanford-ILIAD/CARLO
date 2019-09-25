bf9270da90aeaf15e750f6e8c969b6c172025901
Allan Zhou <8732147+AllanYangZhou@users.noreply.github.com>
2019-09-25 15:13:38 -0700

    Demo collection and bc training

---
+++ demo_collection.py
@@ -0,0 +1,77 @@
+import os
+import time
+from absl import app, flags
+import gym
+import driving_envs  # pylint: disable=unused-import
+import numpy as np
+import pygame
+
+LEFT_X_AXIS = 0
+LEFT_Y_AXIS = 1
+RIGHT_X_AXIS = 3
+RIGHT_Y_AXIS = 4
+
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("folder", "demos", "Folder to save file in.")
+
+
+def main(_argv):
+    folder = FLAGS.folder
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    pygame.init()
+    pygame.joystick.init()
+    joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+    if not joysticks:
+        raise Exception("No joysticks found.")
+    for joystick in joysticks:
+        joystick.init()
+    multi_env = gym.make("Merging-v1", random_initial=True)
+    print("Starting in 3!")
+    done = False
+    _obs = multi_env.reset()
+    multi_env.render()
+    i = 0
+    demonstration_data = []
+    time.sleep(3)
+    while not done:
+        state = multi_env.world.state
+        pygame.event.pump()
+        if len(joysticks) == 1:
+            r_action = (
+                -0.05 * joysticks[0].get_axis(RIGHT_X_AXIS),
+                -4 * joysticks[0].get_axis(RIGHT_Y_AXIS),
+            )
+            h_action = (
+                -0.05 * joysticks[0].get_axis(LEFT_X_AXIS),
+                -4 * joysticks[0].get_axis(LEFT_Y_AXIS),
+            )
+        else:
+            r_action = (
+                -0.05 * joysticks[0].get_axis(RIGHT_X_AXIS),
+                -4 * joysticks[0].get_axis(LEFT_Y_AXIS),
+            )
+            h_action = (
+                -0.05 * joysticks[1].get_axis(RIGHT_X_AXIS),
+                -4 * joysticks[1].get_axis(LEFT_Y_AXIS),
+            )
+        action = np.array(h_action + r_action)
+        _obs, _rew, done, _debug = multi_env.step(action)
+        demonstration_data.append((state, action))
+        multi_env.render()
+        time.sleep(multi_env.dt)
+        i += 1
+    multi_env.world.close()
+    states, actions = [np.stack(x) for x in zip(*demonstration_data)]
+    if len(demonstration_data) != 60:
+        print("WARNING: len(demonstration_data)=={}".format(len(demonstration_data)))
+    time_in_secs = int(time.time())
+    np.savez(
+        os.path.join(folder, "{}.npz".format(time_in_secs)), states=states, actions=actions
+    )
+    return
+
+
+if __name__ == "__main__":
+    app.run(main)
---
+++ train_bc.py
@@ -0,0 +1,66 @@
+"""Train bc policy on folder of demos."""
+import os
+from absl import flags, app
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import load_model
+from tensorflow.keras.initializers import Constant
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("folder", "demos", "Folder to save file in.")
+flags.DEFINE_string("save_name", "model.h5", "Output weights file name.")
+
+
+def main(_argv):
+    folder = FLAGS.folder
+    states, actions = [], []
+    for path in os.listdir(folder):
+        filez = np.load(os.path.join(folder, path))
+        states.append(filez["states"])
+        actions.append(filez["actions"])
+    states = np.concatenate(states, axis=0)
+    actions = np.concatenate(actions, axis=0)
+    h_actions = actions[:, :2]
+
+    state_mean = np.mean(states, axis=0).tolist()
+    state_std = np.std(states, axis=0).tolist()
+    action_mean = np.std(h_actions, axis=0).tolist()
+    action_std = np.std(h_actions, axis=0).tolist()
+
+    def normalize_obs(obs, mean=0, std=1):
+        mean = tf.convert_to_tensor(mean, dtype=tf.float32)
+        std = tf.convert_to_tensor(std, dtype=tf.float32)
+        return (obs - mean) / (1e-6 + std)
+
+    def normalize_action(act, mean=0, std=1):
+        mean = tf.convert_to_tensor(mean, dtype=tf.float32)
+        std = tf.convert_to_tensor(std, dtype=tf.float32)
+        return act * std + mean
+
+    model = keras.Sequential()
+    model.add(
+        layers.Lambda(
+            normalize_obs,
+            arguments={"mean": state_mean, "std": state_std},
+            input_shape=(14,),
+            output_shape=(14,),
+        )
+    )
+    model.add(layers.Dense(64, activation="relu"))
+    model.add(layers.Dense(64, activation="relu"))
+    model.add(layers.Dense(2, activation=None))
+    model.add(
+        layers.Lambda(normalize_action, arguments={"mean": action_mean, "std": action_std})
+    )
+    model.compile(optimizer=keras.optimizers.Adam(0.001), loss="mse", metrics=["mse"])
+    model.fit(states, h_actions, epochs=20, batch_size=32)
+    model.save(FLAGS.save_name)
+    del model
+    model = load_model(FLAGS.save_name)
+    model.evaluate(states, h_actions)
+
+
+if __name__ == "__main__":
+    app.run(main)
